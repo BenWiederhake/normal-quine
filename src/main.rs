54167b4da486ebf5c6786c5a69f2ddeab098b73f
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2017-09-22 17:19:42 +0200

    Avoid nightly feature

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-#![feature(str_escape)]
-
-const THE_MAGIC: &'static str = "#![feature(str_escape)]\n\nconst THE_MAGIC: &\'static str = \"@\";\n\nfn main() {\n    println!(\"{}\", THE_MAGIC.replace(\'\\u{40}\', THE_MAGIC.escape_debug().as_str()));\n}";
+const THE_MAGIC: &'static str = "const THE_MAGIC: &'static str = \"@\";
 
 fn main() {
-    println!("{}", THE_MAGIC.replace('\u{40}', THE_MAGIC.escape_debug().as_str()));
+    println!(\"{}\", THE_MAGIC.replace('\\u{40}', THE_MAGIC.replace(\"\\\\\", \"\\\\\\\\\").replace(\"\\\"\", \"\\\\\\\"\").as_str()));
+}";
+
+fn main() {
+    println!("{}", THE_MAGIC.replace('\u{40}', THE_MAGIC.replace("\\", "\\\\").replace("\"", "\\\"").as_str()));
 }
