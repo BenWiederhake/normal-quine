93cfe1669f22482346236af625f1f4ee8161f76b
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2017-09-22 17:13:55 +0200

    First quine, using feature str_escape

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#![feature(str_escape)]
+
+const THE_MAGIC: &'static str = "#![feature(str_escape)]\n\nconst THE_MAGIC: &\'static str = \"@\";\n\nfn main() {\n    println!(\"{}\", THE_MAGIC.replace(\'\\u{40}\', THE_MAGIC.escape_debug().as_str()));\n}";
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", THE_MAGIC.replace('\u{40}', THE_MAGIC.escape_debug().as_str()));
 }
